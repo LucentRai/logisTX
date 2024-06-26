3b7708b173a1238e0453ae454d4aa1f90b9e8a0c
LucentRai <lucentthulung@gmail.com>
2024-06-26 06:48:01 +0545

    added fields to models object

--- server/dev-tools/import-delete.js
+++ server/dev-tools/import-delete.js
@@ -28,17 +28,41 @@ if(options.server) {
 	databaseServer = process.env.DB.replace('<password>', process.env.DB_PASSWORD);
 }
 
-var model = {
-	company,
-	customerModel,
-	orderModel,
-	productModel,
-	transportModel,
-	warehouseModel
+var models = {
+	company: {
+		names: ['company', 'companies'],
+		modelFile: 'companyModel',
+		dataFile: 'companies.json'
+	},
+	customer: {
+		names: ['customer', 'customers'],
+		modelFile: 'customerModel',
+		dataFile: 'customers.json'
+	},
+	order: {
+		names: ['order', 'orders'],
+		modelFile: 'orderModel',
+		dataFile: 'orders.json'
+	},
+	product: {
+		names: ['product', 'products'],
+		modelFile: 'productModel',
+		dataFile: 'products.json'
+	},
+	transport: {
+		names: ['transport', 'transports'],
+		modelFile: 'transportModel',
+		dataFile: 'transports.json'
+	},
+	warehouse: {
+		names: ['warehouse', 'warehouses'],
+		modelFile: 'warehouseModel',
+		dataFile: 'warehouses.json'
+	},
 };
 
 const inputDescription = `Import a collection into the database;
-Collections include ${Object.keys(model).join(', ')}.
+Collections include ${Object.keys(models).join(', ')}.
 If no collection is mentioned, all collections will be imported`;
 
 program.command('import')
@@ -49,7 +73,7 @@ program.command('import')
 });
 
 const deleteDescription = `Delete all data of collection from the database
-Collections includes ${Object.keys(model).join(', ')}.
+Collections includes ${Object.keys(models).join(', ')}.
 If no collection is mentioned, all collections will be deleted`;
 
 program.command('delete')
@@ -75,50 +99,50 @@ async function importData(collections){
 			switch(collection) {
 				case 'company': case 'companies':
 					const companies = JSON.parse(fs.readFileSync(`${__dirname}/data/companies.json`, 'utf-8'));
-					model.company = require('../models/companyModel');
-					await model.company.create(companies);
+					models.company = require('../models/companyModel');
+					await models.company.create(companies);
 					console.log('Companies imported successfully');
 					break;
 
 				case 'customer': case 'customers':
 					const customers = JSON.parse(fs.readFileSync(`${__dirname}/data/customers.json`, 'utf-8'));
-					model.customer = require('../models/customerModel');
-					await model.customer.create(customers);
+					models.customer = require('../models/customerModel');
+					await models.customer.create(customers);
 					console.log('Customers imported successfully');
 					break;
 
 				case 'order': case 'orders':
 					const orders = JSON.parse(fs.readFileSync(`${__dirname}/data/orders.json`, 'utf-8'));
-					model.order = require('../models/orderModel');
-					await model.order.create(orders);
+					models.order = require('../models/orderModel');
+					await models.order.create(orders);
 					console.log('Orders imported successfully');
 					break;
 
 				case 'product': case 'products':
 					const products = JSON.parse(fs.readFileSync(`${__dirname}/data/products.json`, 'utf-8'));
-					model.product = require('../models/productModel');
-					await model.product.create(products);
+					models.product = require('../models/productModel');
+					await models.product.create(products);
 					console.log('Products imported successfully');
 					break;
 
 				case 'transport': case 'transports':
 					const transports = JSON.parse(fs.readFileSync(`${__dirname}/data/transports.json`, 'utf-8'));
-					model.transport = require('../models/transportModel');
-					await model.transport.create(transports);
+					models.transport = require('../models/transportModel');
+					await models.transport.create(transports);
 					console.log('Transports imported successfully');
 					break;
 
 				case 'user': case 'users':
 					const users = JSON.parse(fs.readFileSync(`${__dirname}/data/users.json`, 'utf-8'));
-					model.user = require('../models/userModel');
-					await model.user.create(users);
+					models.user = require('../models/userModel');
+					await models.user.create(users);
 					console.log('Users imported successfully');
 					break;
 
 				case 'warehouse': case 'warehouses':
 					const warehouses = JSON.parse(fs.readFileSync(`${__dirname}/data/warehouses.json`, 'utf-8'));
-					model.warehouse = require('../models/warehouseModel');
-					await model.warehouse.create(warehouses);
+					models.warehouse = require('../models/warehouseModel');
+					await models.warehouse.create(warehouses);
 					console.log('Warehouses imported successfully');
 					break;
 
@@ -133,4 +157,113 @@ async function importData(collections){
 	finally{
 		process.exit();
 	}
+}
+
+async function deleteData(collections) {
+	try {
+		requireAllJSON();
+		if(collections.includes('all')) {
+			console.log('Deleting all data');
+			await models.company.deleteMany();
+			console.log('Companies data deleted');
+			await models.customer.deleteMany();
+			console.log('Customers data deleted');
+			await models.order.deleteMany();
+			console.log('Orders data deleted');
+			await models.product.deleteMany();
+			console.log('Products data deleted');
+			await models.transport.deleteMany();
+			console.log('Tranports data deleted');
+			await models.user.deleteMany();
+			console.log('Users data deleted');
+			await models.warehouse.deleteMany();
+			console.log('Warehouses data deleted');
+			process.exit();
+		}
+		for(const collection of collections) {
+			switch(collection) {
+				case 'companies': case 'company':
+					await models.company.deleteMany();
+					console.log('Companies data deleted successfully');
+					break;
+
+				case 'customers': case 'customer':
+					await models.customer.deleteMany();
+					console.log('Customers data deleted successfully');
+					break;
+
+				case 'order': case 'order':
+					await models.order.deleteMany();
+					console.log('Orders data deleted successfully');
+					break;
+
+				case 'products': case 'product':
+					await models.product.deleteMany();
+					console.log('Products data deleted successfully');
+					break;
+
+				case 'transports': case 'transport':
+					await models.transport.deleteMany();
+					console.log('Transports data deleted successfully');
+					break;
+
+				case 'users': case 'user':
+					await models.user.deleteMany();
+					console.log('Users data deleted successfully');
+					break;
+
+					case 'warehouses': case 'warehouse':
+						await models.warehouse.deleteMany();
+						console.log('Warehouses data deleted successfully');
+						break;
+
+				default:
+					throw new Error(`Invalid collection name: ${collection}`);
+			}
+		}
+	}
+	catch(err) {
+		console.error(err);
+	}
+	finally {
+		process.exit();
+	}
+}
+
+
+async function importAllData() {
+	requireAllJSON();
+	const companies = JSON.parse(fs.readFileSync(`${__dirname}/data/companies.json`, 'utf-8'));
+	const customers = JSON.parse(fs.readFileSync(`${__dirname}/data/customers.json`, 'utf-8'));
+
+	await CategoryModel.create(companies);
+	console.log('Categories imported');
+	await DesignationModel.create(designations);
+	console.log('Employee Designations imported');
+	await EmployeeModel.create(employees);
+	console.log('Employees imported');
+	// await ExpenseModel.create(expenses);
+	// console.log('Expenses imported');
+	await FeeModel.create(fees);
+	console.log('Fees imported');
+	await GradeModel.create(grades);
+	console.log('Grades imported');
+	// await ParentModel.create(parents);
+	// console.log('Parents imported');
+	// await RevenueModel.create(revenues);
+	// console.log('Revenues imported');
+	// await StudentModel.create(students);
+	// console.log('Students imported');
+	await UserModel.create(users);
+	console.log('Users imported \n\nAll data imported successfully');
+}
+
+function requireAllJSON() {
+	models.company = require('../models/companyModel');
+	models.customer = require('../models/customerModel');
+	models.order = require('../models/orderModel');
+	models.product = require('../models/productModel');
+	models.transport = require('../models/transportModel');
+	models.user = require('../models/userModel');
+	models.warehouse = require('../models/warehouseModel');
 }
