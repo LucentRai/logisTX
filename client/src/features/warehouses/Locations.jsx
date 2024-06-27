05c73dd35296a1ab048e459f913a5fe12fb8cd0a
LucentRai <lucentthulung@gmail.com>
2024-06-27 17:52:33 +0545

    auto center

--- client/src/features/warehouses/Locations.jsx
+++ client/src/features/warehouses/Locations.jsx
@@ -1,25 +1,37 @@
-import { MapContainer, Marker, TileLayer } from 'react-leaflet';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import { useSelector } from 'react-redux';
 
 import 'leaflet/dist/leaflet.css';
+import SpinnerFullPage from '../../ui/SpinnerFullPage';
 
 function Locations(){
 	const warehouses = useSelector(state => state.warehouses);
 
-	const center1 = warehouses.reduce((acc, warehouse) => {
-		return [acc[0] + warehouse.location[0] / warehouses.length, acc[1] + warehouse.location[1] / warehouses.length];
-	}, [0, 0]);
-	console.log(center1);
+	const center = warehouses.length > 0 ?
+		warehouses.reduce((acc, warehouse) => {
+			return [
+				acc[0] + warehouse.location[0] / warehouses.length,
+				acc[1] + warehouse.location[1] / warehouses.length
+			];
+		}, [0, 0]) 
+		: null;
 
+	
+	if(!center){
+		return <SpinnerFullPage />;
+	}
 
 	return (
 		<div className="col-10">
-			<MapContainer center={[ 27.684966666666668, 85.36236666666667 ]} zoom={13} scrollWheelZoom={false}>
+			<MapContainer center={center} zoom={13} scrollWheelZoom={false}>
 				<TileLayer
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 				/>
 				{warehouses.map(warehouse => (
 					<Marker key={warehouse._id} position={warehouse.location}>
+						<Popup>
+							{warehouse.name}
+						</Popup>
 					</Marker>
 				))}
 			</MapContainer>
