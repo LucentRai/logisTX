a843c06247eb2e74ab4cfdd642131acbc1951e4a
LucentRai <lucentthulung@gmail.com>
2024-07-03 10:22:27 +0545

    added _id to dummy data

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -8,11 +8,11 @@ const factory = require('../controllers/handlerFactory');
 module.exports = {
 	getOrder: factory.getOne(Order, [
 		{path: 'orderItems', select: 'name warehouseId'},
-		{path: 'customerId', select: 'name address'}
+		{path: 'customerId', select: 'name address location'}
 	]),
 	getAllOrders: factory.getAll(Order, [
 		{path: 'orderItems', select: 'name'},
-		{path: 'customerId', select: 'name'}
+		{path: 'customerId', select: 'name location'}
 	]),
 	createOrder: factory.createOne(Order),
 	updateOrder: factory.updateOne(Order),
