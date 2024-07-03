65d68886ed9a581d3914a8608533f3cb0dd94425
LucentRai <lucentthulung@gmail.com>
2024-07-03 10:56:39 +0545

    copy lattitude, longitude values to clipboard

--- client/src/features/maps/MapCoordinates.jsx
+++ client/src/features/maps/MapCoordinates.jsx
@@ -17,31 +17,54 @@
 import { useState } from 'react';
 import { MapContainer, TileLayer, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import {Button, Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
+import toast from 'react-hot-toast';
 
 import { MAP_CENTER } from '../../../../constants.json';
 
 function MapCoordinates() {
+	const [modal, setModal] = useState(false);
 	// Optional: State to store and display coordinates
-	const [coordinates, setCoordinates] = useState(null);
+	const [coordinates, setCoordinates] = useState([]);
 
 	function MapClickHandler() {
 		useMapEvents({
 			click: (e) => {
-				const { lat, lng } = e.latlng;
-				setCoordinates({ lat, lng }); // Update state with clicked coordinates
-				alert(`Latitude: ${lat}, Longitude: ${lng}`); // Or use this to show coordinates immediately
+				setCoordinates([ e.latlng.lat, e.latlng.lng ]); // Update state with clicked coordinates
+				setModal(!modal);
 			},
 		});
 		return null;
 	}
 
+	function copyCoordinatesToClipboard() {
+		const coordinatesStr = `${coordinates[0]}, ${coordinates[1]}`;
+		navigator.clipboard.writeText(coordinatesStr)
+			.then(() => {
+				toast.success('Coordinates copied to clipboard');
+			})
+			.catch(err => {
+				console.error('Failed to copy coordinates: ', err);
+			});
+	}
+
 	return (
-		<MapContainer center={MAP_CENTER} zoom={13} scrollWheelZoom={false}>
-			<TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-			<MapClickHandler />
-			{/* Optionally render coordinates on the page */}
-			{coordinates && <p>Clicked at Latitude: {coordinates.lat}, Longitude: {coordinates.lng}</p>}
-		</MapContainer>
+		<>
+			<Modal isOpen={modal} toggle={MapClickHandler}>
+				<ModalHeader toggle={() => setModal(!modal)}>Coordinates</ModalHeader>
+				<ModalBody>Lattitude: {coordinates[0]}<br />Longitude: {coordinates[1]}</ModalBody>
+				<ModalFooter>
+					<Button color="primary" onClick={copyCoordinatesToClipboard}>Copy</Button>
+					<Button color="secondary" onClick={() => setModal(!modal)}>Close</Button>
+				</ModalFooter>
+			</Modal>
+			<MapContainer center={MAP_CENTER} zoom={13} scrollWheelZoom={false}>
+				<TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+				<MapClickHandler />
+				{/* Optionally render coordinates on the page */}
+				{coordinates && <p>Clicked at Latitude: {coordinates.lat}, Longitude: {coordinates.lng}</p>}
+			</MapContainer>
+		</>
 	);
 }
 
